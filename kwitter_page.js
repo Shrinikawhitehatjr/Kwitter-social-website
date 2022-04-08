7fcb921a7ea093bd90460c19da7f0aac1908ff6d
Shrinikawhitehatjr <97172285+Shrinikawhitehatjr@users.noreply.github.com>
2022-04-08 19:30:20 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,31 @@
+//YOUR FIREBASE LINKS
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+         function redirectToRoomName(name)
+         {
+             console.log(name);
+             localStorage.setItem("room_name" , name);
+             window.location = "kwitter_page.html";
+         }
+
+//Start code
+   function logout() {
+         localStorage.removeItem("user_name");
+         localStorage.removeItem("room_name");
+         window.location = "kwitter.html";
+   }
+   function send()
+   {
+         msg = document.getElementById("msg").value;
+         firebase.database().ref(room_name).push({
+           name:user_name,
+           Message:msg,
+           like:0
+      });
+      document.getElementById("msg").value = "";
+   }
+//End code
+      } });  }); }
+getData();
